c5feac7b3a9c601ac241f52953097319b5af3e56
valentin <valentin.lion@laposte.net>
2018-03-30 16:03:39 +0200

    Add component pokedex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
-import { AppComponent } from './app.component';
+import {PokedexComponent} from './pokedex/pokedex.component';
+import {AppComponent} from "./app.component";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+      AppComponent,
+      PokedexComponent
   ],
   imports: [
     BrowserModule
-  ],
+      , FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
